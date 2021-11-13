3c171adddf98edaa542c44e586829ae16ac2f26c
puntertram <puneethnaik60@gmail.com>
2021-11-13 20:59:49 +0530

    rudimentary main file to test FetchUnit::fetch()

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <bits/stdc++.h>
+#include "./Core/Frontend/FetchUnit.h"
+
+
 using namespace std;
 int main(int argc, char **argv) {
-    
+    FetchUnit *fetchUnit = new FetchUnit();
+    fetchUnit->fetch();
+
     return 0;
 }
