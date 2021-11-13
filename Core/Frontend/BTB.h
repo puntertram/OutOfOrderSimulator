66caf62e01ad46c2e8ac84df25afe1b3617c7791
puntertram <puneethnaik60@gmail.com>
2021-11-13 20:07:22 +0530

    getTargetAddress impl

---
+++ Core/Frontend/BTB.h
@@ -0,0 +1,18 @@
+#ifndef BTB_H
+
+#define BTB_H 
+#include <map>
+
+using namespace std;
+
+
+class BTB {
+public:
+    map<int, int> buffer;
+    int getTargetAddress() {
+        
+    }
+};
+
+
+#endif
